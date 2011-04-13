0d39fab26daf01a0a522367084d07ad6fc4bcd3f
Russ Bradberry <rbradberry@gmail.com>
2011-04-13 18:11:40 -0400

    make it a non-enumerable property

--- test/node-event-log.test.js
+++ test/node-event-log.test.js
@@ -1,10 +1,17 @@
 /**
  * Module dependencies.
  */
+ /**
+ var EventEmitter = require('events').EventEmitter
+ var EventLogger = require('./')
+ var a = new EventEmitter()
+ var b = new EventEmitter()
+ var logger = new EventLogger()
+ logger.attach(a)
+ */
 var EventLogger = require('node-event-log'),
     EventEmitter = require('events').EventEmitter,
     logger = new EventLogger(),
-    emitter = new EventEmitter(),
     test = module.exports,
     should = require('should');
 
@@ -12,3 +19,16 @@ test['.version'] = function(){
   logger.version.should.match(/^\d+\.\d+\.\d+$/);  
 };
 
+test['.attach'] = function(){
+  var emitter = new EventEmitter();
+  logger.attach(emitter);
+  emitter.should.respondTo('__emit__');
+  emitter.should.respondTo('emit');
+  emitter.emit.should.not.equal(emitter.__emit__);
+};
+
+test['emitter.emit'] = function(){
+  var emitter = new EventEmitter();
+  logger.attach(emitter);
+  emitter.emit('test');
+};
